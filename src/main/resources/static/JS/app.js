aaa1ceca2f35ca5160b263a497dff4002da268bb
unknown <daniela.garcia-r@labinfo.is.escuelaing.edu.co>
2023-03-09 09:17:02 -0500

    Avance

---
+++ src/main/resources/static/JS/app.js
@@ -0,0 +1,12 @@
+var apimock = apimock;
+
+var app = (function (){
+    var author;
+    var blueprintName;
+
+        function getName() {
+                $("#name").text( "Blueprints de: " + author );
+            }
+
+
+}
