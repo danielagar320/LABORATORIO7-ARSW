3cd3d081e1c68108d44a562e659405beda8dd218
unknown <daniela.garcia-r@labinfo.is.escuelaing.edu.co>
2023-03-14 09:06:57 -0500

    Lab finalizado

--- src/main/resources/static/JS/apimock.js
+++ src/main/resources/static/JS/apimock.js
@@ -8,6 +8,10 @@ apimock=(function(){
 	 {author:"Edwar Lozano","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear"}];
 	mockdata["Daniela Garcia"]=[{author:"Daniela Garcia","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
 	 {author:"Daniela Garcia","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
+	mockdata["Fabian Florez"]=	[{author:"Fabian Florez","points":[{"x":134,"y":86},{"x":125,"y":165}],"name":"house"},
+     {author:"Fabian Florez","points":[{"x":75,"y":12},{"x":210,"y":167}],"name":"gear"}];
+    mockdata["Daniela Romero"]=[{author:"Daniela Romero","points":[{"x":137,"y":82},{"x":123,"y":161}],"name":"house2"},
+     {author:"Daniela Romero","points":[{"x":75,"y":12},{"x":210,"y":167}],"name":"gear2"}];
 
 
 	return {
@@ -27,11 +31,3 @@ apimock=(function(){
 
 })();
 
-/*
-Example of use:
-var fun=function(list){
-	console.info(list);
-}
-
-apimock.getBlueprintsByAuthor("johnconnor",fun);
-apimock.getBlueprintsByNameAndAuthor("johnconnor","house",fun);*/
